554ccd6893639f716dcae5763584384cf399714b
Harri Lehtola <harri.lehtola@futurice.com>
2015-10-16 10:55:11 +0200

    Silence linter camel case errors in event model

--- api/models/event.js
+++ api/models/event.js
@@ -7,13 +7,13 @@ var EventSchema = new Schema({
   end_time: Date,       // jshint ignore:line
   location: String,
   description: String,
-  speaker_role: String,
-  bar_camp: Boolean,
-  twitter_handle: String,
-  linkedin_url: String,
+  speaker_role: String, // jshint ignore:line
+  bar_camp: Boolean,    // jshint ignore:line
+  twitter_handle: String, // jshint ignore:line
+  linkedin_url: String, // jshint ignore:line
   day: String,
-  image_url: String,
-  speaker_image_url: String,
+  image_url: String,    // jshint ignore:line
+  speaker_image_url: String, // jshint ignore:line
   artists: String,
   starred_count: Number // jshint ignore:line
 });
